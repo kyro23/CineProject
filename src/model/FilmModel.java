1bd43e3d24c73dace8dc4e5d51a1d953c5bf2a9e
Kyro23 <cassiano.jr.2009@hotmail.com>
2017-07-17 15:14:18 -0300

    Added film Model for package

--- src/model/FilmModel.java
+++ src/model/FilmModel.java
@@ -9,48 +9,56 @@ public class FilmModel {
 	private int duraction;
 	private String directors;
 	private String classindicative;
+	
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getTitle() {
 		return title;
 	}
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
 	public String getActors() {
 		return actors;
 	}
 	public void setActors(String actors) {
 		this.actors = actors;
 	}
+	
 	public String getSinopse() {
 		return sinopse;
 	}
 	public void setSinopse(String sinopse) {
 		this.sinopse = sinopse;
 	}
+	
 	public String getGenere() {
 		return genere;
 	}
 	public void setGenere(String genere) {
 		this.genere = genere;
 	}
+	
 	public int getDuraction() {
 		return duraction;
 	}
 	public void setDuraction(int duraction) {
 		this.duraction = duraction;
 	}
+	
 	public String getDirectors() {
 		return directors;
 	}
 	public void setDirectors(String directors) {
 		this.directors = directors;
 	}
+	
 	public String getClassindicative() {
 		return classindicative;
 	}

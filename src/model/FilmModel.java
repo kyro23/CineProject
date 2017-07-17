ecfc82baf608db54e743b1abaae8e42d8417c3c8
Kyro23 <cassiano.jr.2009@hotmail.com>
2017-07-17 10:07:29 -0300

    Model class for film

---
+++ src/model/FilmModel.java
@@ -0,0 +1,62 @@
+package model;
+
+public class FilmModel {
+	private int id;
+	private String title;
+	private String actors;
+	private String sinopse;
+	private String genere;
+	private int duraction;
+	private String directors;
+	private int classindicative;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getActors() {
+		return actors;
+	}
+	public void setActors(String actors) {
+		this.actors = actors;
+	}
+	public String getSinopse() {
+		return sinopse;
+	}
+	public void setSinopse(String sinopse) {
+		this.sinopse = sinopse;
+	}
+	public String getGenere() {
+		return genere;
+	}
+	public void setGenere(String genere) {
+		this.genere = genere;
+	}
+	public int getDuraction() {
+		return duraction;
+	}
+	public void setDuraction(int duraction) {
+		this.duraction = duraction;
+	}
+	public String getDirectors() {
+		return directors;
+	}
+	public void setDirectors(String directors) {
+		this.directors = directors;
+	}
+	public int getClassindicative() {
+		return classindicative;
+	}
+	public void setClassindicative(int classindicative) {
+		this.classindicative = classindicative;
+	}
+	
+
+}
